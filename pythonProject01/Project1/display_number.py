d41a33ed87ce869e5d78b381278f6fbe992dd67f
İpek Sandal <ipekksandalll@gmail.com>
2024-07-11 13:51:37 +0300

    Update display_number.py
    new

--- pythonProject01/Project1/display_number.py
+++ pythonProject01/Project1/display_number.py
@@ -1,5 +1,5 @@
 def display_number(number):
-    # Her rakamın yedi segmentli ekran karşılığını tanımla
+    
     digits = [
         ["###", "# #", "# #", "# #", "###"],  # 0
         ["  #", "  #", "  #", "  #", "  #"],  # 1
@@ -13,24 +13,24 @@ def display_number(number):
         ["###", "# #", "###", "  #", "###"]  # 9
     ]
 
-    # Girdiyi string olarak al ve her rakamı ayrı ayrı işle
+    
     num_str = str(number)
 
-    # Beş satır için dizileri başlat
+   
     lines = [""] * 5
 
-    # Her rakamı sırayla işle ve uygun satıra ekle
+    
     for digit in num_str:
         d = int(digit)
         for i in range(5):
             lines[i] += digits[d][i] + " "
 
-    # Her satırı yazdır
+  
     for line in lines:
         print(line.rstrip())
 
 
-# Örnek girdi ve çıktı
+
 display_number(123)
 print()
 display_number(9081726354)
